e6b8daa5a49b215e2e7a043346dd81a29c12fbb9
zodaland <me@zodaland.com>
2022-03-30 04:30:41 +0900

    이미지 파일 변경시 state에 반영되도록 수정

--- web/components/Img.tsx
+++ web/components/Img.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Image, { ImageLoaderProps } from 'next/image';
 
 import Loading from './Loading';
@@ -10,6 +10,10 @@ const Img = ({ file }: ImageProps) => {
     const [isLoad, setIsLoad] = useState<boolean>(false);
 
     const loader = ({ src }: ImageLoaderProps) => `${process.env.NEXT_PUBLIC_IMAGE_URL}${src}`;
+
+    useEffect(() => {
+        setSrc(file);
+    }, [file]);
     return (
         <>
             <div className="relative w-full h-full">
