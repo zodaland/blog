566335b381a268ae80a97cdbfe4bb930483a1790
zodaland <me@zodaland.com>
2022-05-20 18:24:36 +0900

    update resume

--- web/pages/resume.tsx
+++ web/pages/resume.tsx
@@ -12,10 +12,7 @@ const ResumePage = () => {
             </Head>
             <div className="flex justify-center">
                 <div className="main resume">
-                    <h1 className="text-5xl font-bold mt-10 mb-14">
-                        저는<span className="text-sky-300">,</span>
-                    </h1>
-                    <div className="flex md:flex-row flex-col">
+                    <div className="flex md:flex-row flex-col mt-10">
                         <div className="flex justify-center md:w-2/6 w-full h-60">
                             <div className="w-60 h-full rounded-full overflow-hidden">
                                 <Img file="profile.jpg" />
@@ -71,6 +68,9 @@ const ResumePage = () => {
                             </div>
                         </div>
                     </div>
+                    <h1 className="text-5xl font-bold mt-10 mb-14 md:ml-0 ml-1">
+                        Intruduce<span className="text-sky-300">,</span>
+                    </h1>
                     <div className="mt-8 pl-2">
                         <ul className="ml-7 list-square list-outside divide-y-8 divide-transparent">
                             <li className="mt-5 ml-5 mr-2 md:text-2xl text-xl">
@@ -88,8 +88,34 @@ const ResumePage = () => {
                             </li>
                         </ul>
                     </div>
-                    <h1 className="text-5xl font-bold mt-28 mb-14">
-                        해 왔던 것은<span className="text-sky-300">,</span>
+                    <h1 className="text-5xl font-bold mt-28 mb-14 md:ml-0 ml-1">
+                        Experience<span className="text-sky-300">,</span>
+                    </h1>
+                    <div className="divide-y md:ml-0 ml-3">
+                        <div className="flex md:flex-row flex-col">
+                            <div className="md:w-1/4 w-full">
+                                <p className="text-3xl font-bold">코리아센터</p>
+                                <p className="mt-4">2020.04 - 2022.</p>
+                                <p className="leading-3 font-thin">Web Developer</p>
+                            </div>
+                            <div className="md:w-3/4 w-full">
+                                <p className="h-9 pt-4 font-thin">연 매출 3400억의 중견 IT기업</p>
+                                <ul className="mt-4 ml-5 list-square list-outside divide-y-4 divide-transparent">
+                                    <li>RFC 5730(EPP)표준의 백엔드 도메인 시스템 개발</li>
+                                    <li>확장성 및 유지보수성 향상을 위한 레거시 시스템 개선</li>
+                                    <li>2021년 8월 부터 메인 개발자로 개발, 배포, 운영을 전담</li>
+                                    <li>도메인 및 DNS 시스템의 전반적인 관리와 유지보수</li>
+                                    <li>
+                                        SQL 튜닝 및 인덱스 설정을 통한 정산 시스템 로딩 시간 평균
+                                        1.51초에서 0.36초로 성능 개선
+                                    </li>
+                                    <li>DNS 설정 자동화 API 개발</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <h1 className="text-5xl font-bold mt-28 mb-14 md:ml-0 ml-1">
+                        Project<span className="text-sky-300">,</span>
                     </h1>
                     <div className="divide-y md:mx-0 mx-3">
                         <div className="flex md:flex-row flex-col mb-8">
@@ -108,21 +134,30 @@ const ResumePage = () => {
                                     </li>
                                     <li className="ml-5">
                                         Nginx, Node.js, php-fpm, Java/Spring, MySQL, MongoDB, Redis,
-                                        Jenkins, Certbot, Mail Server/Client 등 구축 및 운영
+                                        Jenkins, Certbot, Mail Server/Client 등의 컨테이너 구축 및
+                                        운영 경험
+                                    </li>
+                                    <li className="ml-5">
+                                        Docker Compose를 사용한 다중 컨테이너 관리
                                     </li>
                                     <li>
                                         서버, WAS, 서비스 환경 3단계의 로그 관리와 메일 발송 및 알람
                                         처리를 통한 이슈 관리
                                     </li>
                                     <li>
-                                        실제 공격(SSH Brute force Attack, kdevtmpfsi)을 바탕으로
-                                        기른 보안 개념과 iptables 관리, 서버 OTP 로그인 적용
+                                        실제 공격(SSH Brute force Attack, kdevtmpfsi, SYN Flooding)을 바탕으로
+                                        기른 보안 개념
+                                    </li>
+                                    <li className="ml-5">iptables 관리</li>
+                                    <li className="ml-5">Nmap 스캔을 이용한 보안 강화</li>
+                                    <li className="ml-5">
+                                        Jenkins를 이용한 서버 OTP 로그인 인증 환경 구성
                                     </li>
                                     <li>
-                                        Jenkins, ShellScript 구성으로 웹사이트 인증서 자동 갱신 환경
-                                        구축
+                                        Jenkins, ShellScript를 사용한 Let's Encrypt 와일드카드
+                                        도메인 인증서 자동 갱신 환경 구축
                                     </li>
-                                    <li>Github Action, Jenkins를 기반으로 자동 배포 CI/CD 구성</li>
+                                    <li>Github Action, Jenkins를 통한 CI/CD 구성</li>
                                     <li>자체 DNS / 네임서버 구성 및 운영</li>
                                 </ul>
                             </div>
@@ -141,13 +176,12 @@ const ResumePage = () => {
                                 </p>
                                 <ul className="mt-4 ml-5 list-square list-outside divide-y-4 divide-transparent">
                                     <li>개인 블로그 개발 및 운영</li>
-                                    <li>로그 및 에러 알람 메일을 통한 적극적인 이슈 관리</li>
                                     <li>SEO 최적화 및 구글 검색엔진 등록</li>
                                     <li>TypeScript 기반의 백엔드 / 프론트엔드 개발 전담</li>
                                     <li>기술 스택</li>
                                     <li className="ml-5">
-                                        TypeScript, NestJS, MySQL, TypeORM, Next.js, Redux, TOAST
-                                        UI, reCAPTCHA, SWR, winston
+                                        TypeScript, NestJS, Jest, MySQL, TypeORM, Next.js, Redux,
+                                        TOAST UI, reCAPTCHA, SWR, winston
                                     </li>
                                 </ul>
                             </div>
@@ -184,7 +218,7 @@ const ResumePage = () => {
                         <div className="flex md:flex-row flex-col mb-8 pt-8">
                             <div className="md:w-1/4 w-full">
                                 <p className="text-3xl font-bold">MollTalk</p>
-                                <p className="mt-4">2021.02 - 2022.04</p>
+                                <p className="mt-4">2021.02 - 2021.04</p>
                                 <p className="leading-3 font-thin">Main Developer</p>
                             </div>
                             <div className="md:w-3/4 w-full">
@@ -199,78 +233,26 @@ const ResumePage = () => {
                                         웹 소켓을 통한 CLI 접속 환경 및 실시간 채팅, 알림, 초대 기능
                                         구현
                                     </li>
-                                    <li>테스트 코드 도입으로 안정성 향상</li>
-                                    <li className="ml-5">coverage: 49.02%</li>
+                                    <li>테스트를 통한 코드 안정성 향상</li>
                                     <li>JavaScript 기반의 백엔드 / 프론트엔드 개발 전담</li>
                                     <li>기술 스택</li>
                                     <li className="ml-5">
-                                        JavaScript, Express.js, Mongoose, MongoDB, MySQL, WebSocket,
-                                        React, Recoil
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
-                        <div className="flex md:flex-row flex-col mb-8 pt-8">
-                            <div className="md:w-1/4 w-full">
-                                <p className="text-3xl font-bold">인덕대학교</p>
-                                <p className="mt-4">2013.03 - 2020.02</p>
-                                <p className="leading-3 font-thin">Graduate</p>
-                            </div>
-                            <div className="md:w-3/4 w-full">
-                                <p className="h-9 pt-4 font-thin">컴퓨터소프트웨어학과</p>
-                                <ul className="mt-4 ml-5 list-square list-outside divide-y-4 divide-transparent">
-                                    <li>
-                                        3년간 공부하며 컴퓨터구조, OS, 서버, 네트워크, 자료구조, 웹
-                                        프로그래밍 등의 과목을 공부했습니다.
-                                    </li>
-                                    <li>
-                                        재학 중 2년 간의 여러가지 아르바이트, 1년 반 동안의 공사
-                                        현장 일을 병행하다보니 졸업이 다소 늦어졌습니다.
+                                        JavaScript, Express.js, Jest, Mongoose, MongoDB, MySQL,
+                                        WebSocket, React, Recoil
                                     </li>
                                 </ul>
                             </div>
                         </div>
                     </div>
-                    <h1 className="text-5xl font-bold mt-28 mb-14">
-                        해 왔던 일은<span className="text-sky-300">,</span>
-                    </h1>
-                    <div className="divide-y md:ml-0 ml-3">
-                        <div className="flex md:flex-row flex-col">
-                            <div className="md:w-1/4 w-full">
-                                <p className="text-3xl font-bold">코리아센터</p>
-                                <p className="mt-4">2020.04 - 2022.</p>
-                                <p className="leading-3 font-thin">Web Developer</p>
-                            </div>
-                            <div className="md:w-3/4 w-full">
-                                <p className="h-9 pt-4 font-thin">연 매출 1700억의 중견 IT기업</p>
-                                <ul className="mt-4 ml-5 list-square list-outside divide-y-4 divide-transparent">
-                                    <li>PHP / CodeIgniter / MySQL 환경에서의 개발 경험</li>
-                                    <li>JAVA 환경에서의 RFC 5730(EPP)표준에 따른 개발 경험</li>
-                                    <li>
-                                        2021년 8월 부터 2022년 3월까지 시스템 혼자서 개발 / 배포 /
-                                        운영 담당
-                                    </li>
-                                    <li>3만 건의 도메인 및 14만 건의 DNS 관리 경험</li>
-                                    <li>
-                                        정산 시스템 페이지 로딩 시간 평균 1.51초에서 0.36초로 성능
-                                        개선
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                    <h1 className="text-5xl font-bold mt-28 mb-14">
-                        할 수 있는 것은<span className="text-sky-300">,</span>
+                    <h1 className="text-5xl font-bold mt-28 mb-14 md:ml-0 ml-1">
+                        Skill<span className="text-sky-300">,</span>
                     </h1>
                     <ul className="mt-4 md:ml-6 ml-8 list-square list-outside divide-y-8 divide-transparent">
                         <li className="text-2xl font-bold">Language</li>
                         <ul className="grid grid-cols-3 list-square list-outside md:ml-10 ml-5 mb-5">
-                            <li className="text-xl font-thin md:ml-10 ml-5 mt-2 border-0">
-                                JavaScript
-                            </li>
                             <li className="text-xl font-thin md:ml-10 ml-5 mt-2">TypeScript</li>
-                            <li className="text-xl font-thin md:ml-10 ml-5 mt-2">PHP</li>
                             <li className="text-xl font-thin md:ml-10 ml-5 mt-2">Java</li>
+                            <li className="text-xl font-thin md:ml-10 ml-5 mt-2">PHP</li>
                             <li className="text-xl font-thin md:ml-10 ml-5 mt-2">
                                 Shell&nbsp;script
                             </li>
@@ -283,9 +265,9 @@ const ResumePage = () => {
                                 NestJS
                             </li>
                             <li className="text-xl font-thin md:ml-10 ml-5 mt-2">Express.js</li>
+                            <li className="text-xl font-thin md:ml-10 ml-5 mt-2">Spring Boot</li>
                             <li className="text-xl font-thin md:ml-10 ml-5 mt-2">TypeORM</li>
                             <li className="text-xl font-thin md:ml-10 ml-5 mt-2">CodeIgniter</li>
-                            <li className="text-xl font-thin md:ml-10 ml-5 mt-2">Laravel</li>
                         </ul>
                         <li className="md:ml-10 ml-5 text-2xl">Front End</li>
                         <ul className="grid grid-cols-3 list-square list-outside md:ml-10 ml-5 mb-5">
@@ -313,46 +295,31 @@ const ResumePage = () => {
                             <li className="md:col-span-1 col-span-2 text-xl font-thin md:ml-10 ml-5 mt-2">
                                 Websocket/Socket.io
                             </li>
-                            <li className="md:col-span-2 col-span-3 text-xl font-thin md:ml-10 ml-5 mt-2">
-                                Extensible Provisioning Protocol(EPP)
-                            </li>
                         </ul>
                     </ul>
-                    <h1 className="text-5xl font-bold mt-28 mb-14">
-                        할 수 있는 일은<span className="text-sky-300">,</span>
+                    <h1 className="text-5xl font-bold mt-28 mb-14 md:ml-0 ml-1">
+                        Education<span className="text-sky-300">,</span>
                     </h1>
-                    <div className="divide-y">
-                        <div className="mb-12">
-                            <p className="text-3xl font-bold">Infra</p>
-                            <ul className="mt-4 md:ml-5 ml-7 list-square list-outside divide-y-4 divide-transparent text-lg font-thin">
-                                <li>
-                                    Name server 구축 및 운영, was / 네트워크 설정 등 인프라 관리
-                                </li>
-                                <li>
-                                    iptables 관리 및 GeoIP 설정, Nmap 사용, 서버 OTP 적용 등 보안
-                                    관리
-                                </li>
-                                <li>
-                                    비효율 적인 프로세스 개선 및 Shell script를 통한 관리 자동화
-                                </li>
-                                <li>로그관리 및 모니터링</li>
-                            </ul>
-                        </div>
-                        <div className="mb-12 pt-12">
-                            <p className="text-3xl font-bold">Docker Environments</p>
-                            <ul className="mt-4 md:ml-5 ml-7 list-square list-outside divide-y-4 divide-transparent text-lg font-thin">
-                                <li>Docker 환경 구성 및 개발</li>
-                                <li>Docker compose 설정을 통한 다중 컨테이너 앱 관리</li>
-                                <li>컨테이너간 통신 및 다수 컨테이너의 통신 환경 구성</li>
-                            </ul>
-                        </div>
-                        <div className="mb-12 pt-12">
-                            <p className="text-3xl font-bold">DevOps</p>
-                            <ul className="mt-4 md:ml-5 ml-7 list-square list-outside divide-y-4 divide-transparent text-lg font-thin">
-                                <li>Jenkins, github을 이용한 CI 환경 구성</li>
-                                <li>Jenkins를 이용한 OTP 인증 환경 구성</li>
-                                <li>SSL 인증서 자동 연장 환경 구성</li>
-                            </ul>
+                    <div className="divide-y md:mx-0 mx-3 mb-20">
+                        <div className="flex md:flex-row flex-col mb-8 pt-8">
+                            <div className="md:w-1/4 w-full">
+                                <p className="text-3xl font-bold">인덕대학교</p>
+                                <p className="mt-4">2013.03 - 2020.02</p>
+                                <p className="leading-3 font-thin">Graduate</p>
+                            </div>
+                            <div className="md:w-3/4 w-full">
+                                <p className="h-9 pt-4 font-thin">컴퓨터소프트웨어학과</p>
+                                <ul className="mt-4 ml-5 list-square list-outside divide-y-4 divide-transparent">
+                                    <li>
+                                        3년간 컴퓨터구조, OS, 서버, 네트워크, 자료구조, 웹
+                                        프로그래밍 등의 과목을 공부했습니다.
+                                    </li>
+                                    <li>
+                                        재학 중 2년 간의 여러가지 아르바이트, 1년 반 동안의 공사
+                                        현장 일을 병행하다보니 졸업이 다소 늦어졌습니다.
+                                    </li>
+                                </ul>
+                            </div>
                         </div>
                     </div>
                 </div>
