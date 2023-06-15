6d0e5a491405cf05ebb251ab514b45b56899534a
zodaland <admin@zodaland.com>
2023-06-16 07:45:53 +0900

    resume update

--- web/pages/resume.tsx
+++ web/pages/resume.tsx
@@ -74,12 +74,12 @@ const ResumePage = () => {
                     <div className="mt-8 pl-2">
                         <ul className="ml-7 list-square list-outside divide-y-8 divide-transparent">
                             <li className="mt-5 ml-5 mr-2 md:text-2xl text-xl">
-                                3년차 개발자로서 <span className="hl">도메인 시스템</span>을 개발,
-                                배포 및 운영했습니다.
+                                3년차 개발자로서 <span className="hl">도메인 네임 시스템</span>을
+                                개발, 배포 및 운영했습니다.
                             </li>
                             <li className="mt-5 ml-5 mr-2 md:text-2xl text-xl">
-                                서버 관리부터 다양한 프로젝트, 배포를 진행하여 전체적인 프로젝트
-                                생명주기 및 비즈니스 모델 성장에 대한 이해력을 길렀습니다.
+                                서버 관리부터 다양한 프로젝트를 경험하여 전체적인 프로젝트 생명주기
+                                및 비즈니스 모델 성장에 대한 이해력을 길렀습니다.
                             </li>
                             <li className="mt-5 ml-5 mr-2 md:text-2xl text-xl">
                                 최근에는 <span className="hl">코어 통신 시스템 개편 프로젝트</span>
@@ -87,9 +87,9 @@ const ResumePage = () => {
                                 <span className="hl">93%</span> 개선하였습니다.
                             </li>
                             <li className="mt-5 ml-5 mr-2 md:text-2xl text-xl">
-                                어떠한 환경에서든 빠르게 적응하며 능동적으로 업무를 수행할 수
-                                있습니다. 또한, 좋은 동료가 되기 위해 소통에 관심을 가지고
-                                노력합니다.
+                                3년간 3곳의 팀 경험을 통해 어떠한 환경에서든 빠르게 적응하며
+                                능동적으로 업무를 수행할 수 있습니다. 또한, 좋은 동료가 되기 위해
+                                소통에 관심을 가지고 노력합니다.
                             </li>
                         </ul>
                     </div>
@@ -126,33 +126,32 @@ const ResumePage = () => {
                                     <div className="mt-2">
                                         <ul className="ml-5 list-square list-outside divide-y-4 divide-transparent">
                                             <li>
-                                                23개의 레거시 분산 모듈을 하나의 시스템으로
+                                                23개의 레거시 분산 모듈을 하나의 시스템으로 아키텍쳐
                                                 <span className="hl">재설계 및 개발 전담</span>
                                             </li>
                                             <li>
-                                                3개의 SDK와 11개의 요청 서버, 최대 24개의 커맨드
-                                                처리를 위한 파사드 패턴 도입으로 보일러 플레이트
-                                                코드 대폭 감소
+                                                <a
+                                                    href="https://datatracker.ietf.org/doc/html/rfc5730"
+                                                    target="_blank"
+                                                >
+                                                    <span className="hl">
+                                                        <u>RFC 5730 (EPP) 프로토콜</u>
+                                                    </span>
+                                                </a>
+                                                과 11개의 각기 다른 서버 통신을 위한 인터페이스
+                                                구현, <span className="hl">코드 베이스</span> /{' '}
+                                                <span className="hl">
+                                                    보일러 플레이트 코드 개선
+                                                </span>
                                             </li>
                                             <li>
-                                                SDK 마다 다른 명령어를 하나의 시스템에서 처리하기
-                                                위한 커맨드 패턴, XML 확장성 공급을 위한 전략 패턴
-                                                도입
+                                                프로토콜에 따른 내부 TCP 통신, 데이터 공통 처리를
+                                                위한 외부 RESTful API 구현
                                             </li>
                                             <li>
-                                                BlockingQueue를 이용한{' '}
-                                                <span className="hl">세션 풀</span> 및{' '}
-                                                <span className="hl">스레드 동기화</span>
-                                                구현
-                                            </li>
-                                            <li>
-                                                TCP 소켓 통신 모듈에서 간단한 REST로 변경, API
-                                                명세서 작성
-                                            </li>
-                                            <li>
-                                                리셀러 활성화를 위해 Spring Security를 도입, 내부
-                                                통신에서 IP 화이트 리스트, 역할, Basic
-                                                Authentication 기반의 외부 통신 API로 변경
+                                                <span className="hl">세션 풀 구현</span>,{' '}
+                                                <span className="hl">동시성 문제 해결</span>로 병렬
+                                                처리 구현
                                             </li>
                                             <li>
                                                 평균 응답속도 <span className="hl">43.31%</span>,
@@ -173,14 +172,17 @@ const ResumePage = () => {
                                     <div className="mt-2">
                                         <ul className="ml-5 list-square list-outside divide-y-4 divide-transparent">
                                             <li>
-                                                수동 DNS 기능 전체 호환을 위한{' '}
+                                                수동 DNS 기능 자동화를 위한{' '}
                                                 <span className="hl">DB, 구조 설계 및 개발</span>
                                             </li>
                                             <li>
-                                                서비스 전반에 분산되어 있던 함수 기반의 기능을
-                                                <span className="hl">클래스 기반</span>으로 작성
+                                                서비스 전반에 분산되어 있던 레거시 코드를
+                                                <span className="hl">객체지향적 코드</span>로 개선
                                             </li>
-                                            <li>사용자, 관리자 화면 작성</li>
+                                            <li>
+                                                기능 전체의 <span className="hl">BE</span> /{' '}
+                                                <span className="hl">FE</span> 역할 수행
+                                            </li>
                                             <li>
                                                 시스템 통합 및 자동화로 운영팀 및 개발팀 업무 효율성
                                                 상승
@@ -207,62 +209,62 @@ const ResumePage = () => {
                                 <ul className="mt-4 ml-5 list-square list-outside divide-y-4 divide-transparent">
                                     <li>서버 세팅부터 개발, 관리 등 전반적인 운영</li>
                                     <li>
-                                        최소한의 서버 구성과 <span className="hl">Docker</span>{' '}
-                                        컨테이너 사용을 최대화한 인프라 구성
+                                        <span className="hl">Docker</span> 기반의 인프라 구성
                                     </li>
                                     <li className="ml-5">
                                         Nginx, Java/Spring, Node.js, php-fpm, MySQL, MongoDB, Redis,
                                         Jenkins, Certbot, Mail Server/Client 등의{' '}
-                                        <span className="hl">컨테이너 구축 및 운영</span> 경험
+                                        <span className="hl">컨테이너 사용 경험</span>
                                     </li>
                                     <li className="ml-5">
                                         <span className="hl">Docker Compose</span>를 사용한 다중
                                         컨테이너 관리
                                     </li>
                                     <li>
-                                        서버, WAS, 서비스 각각의 로그 관리와 자동 알람 처리를 통한
-                                        이슈 관리
+                                        서버 공격(SSH Brute force Attack, kdevtmpfsi, SYN Flooding
+                                        등) 대응 경험
                                     </li>
-                                    <li>
-                                        실제 공격(SSH Brute force Attack, kdevtmpfsi, SYN Flooding
-                                        등)을 받은 경험을 바탕으로 기른 보안 개념
-                                    </li>
-                                    <li className="ml-5">방화벽 관리, Nmap을 이용한 보안 강화</li>
+                                    <li className="ml-5">방화벽, Nmap을 이용한 보안 강화</li>
                                     <li className="ml-5">
-                                        <span className="hl">Jenkins</span>와 Webhook 이용한 서버
-                                        OTP 로그인 인증 환경 구성
+                                        Jenkins를 이용한 <span className="hl">2차 인증 구현</span>{' '}
+                                        (OTP)
                                     </li>
                                     <li>
-                                        Jenkins, ShellScript를 사용한 Let's Encrypt 와일드카드
-                                        도메인 인증서 자동 갱신 환경 구축
+                                        자체 DNS / 네임서버 구축 및 와일드카드 도메인 인증서 자동
+                                        갱신 구현
                                     </li>
                                     <li>
-                                        Github Action, Jenkins를 통한{' '}
+                                        Github Action, Jenkins를 이용한
                                         <span className="hl">CI/CD</span> 구성
                                     </li>
-                                    <li>자체 DNS / 네임서버 구성 및 운영</li>
                                 </ul>
                             </div>
                         </div>
                         <div className="flex md:flex-row flex-col mb-4 pt-8">
                             <div className="md:w-1/4 w-full">
-                                <p className="text-3xl font-bold">마륜정</p>
+                                <p className="text-3xl font-bold">웹 잡지</p>
                                 <p className="mt-4">2022.08 - 2023.</p>
                                 <p className="leading-3 font-thin">Main Developer</p>
                             </div>
                             <div className="md:w-3/4 w-full">
                                 <p className="h-9 pt-4 font-thin"></p>
                                 <ul className="mt-4 ml-5 list-square list-outside divide-y-4 divide-transparent">
-                                    <li>온라인 잡지 상품 판매 쇼핑몰</li>
+                                    <li>
+                                        <a href="https://naver.me/xXDM8Fy6" target="_blank">
+                                            <span className="hl">
+                                                <u>
+                                                    비즈니스 모델에 대한 관심을 바탕으로 웹
+                                                    애플리케이션 개발
+                                                </u>
+                                            </span>
+                                        </a>
+                                    </li>
                                     <li>백엔드 / 프론트엔드 개발 전담</li>
                                     <li>
-                                        개별 인증을 통한 일회성 접근 URL과 IAM, S3를 통한 이미지와
-                                        1:1 매핑되는 pre-signed URL을 이용해 웹 잡지 상품 구현
+                                        개별 인증을 통한 일회성 접근 URL과 한시적 이미지를 이용한 웹
+                                        잡지 상품 구현
                                     </li>
-                                    <li>
-                                        스크래핑을 통해 매 주 갱신되는 일정 정보에 따른 자동 상품
-                                        갱신 시스템 구축
-                                    </li>
+                                    <li>주기적 일정 정보에 따른 자동 상품 갱신 시스템 구축</li>
                                     <li>기술 스택</li>
                                     <li className="ml-5">BackEnd</li>
                                     <li className="ml-10">
@@ -297,13 +299,10 @@ const ResumePage = () => {
                                     <li>
                                         카테고리, 태그의 추가 / 수정이 용이한 블로그 페이지 구현
                                     </li>
+                                    <li>개인 메일서버 연동, 2차 보안 관리자 페이지 구현</li>
                                     <li>
-                                        개인 메일서버를 연동한 컨택트 페이지와 OTP + 패스워드 보안의
-                                        관리자 페이지 구현
-                                    </li>
-                                    <li>
-                                        SEO 최적화 및 구글 검색엔진 등록, 2023년 2월 기준{' '}
-                                        <span className="hl">MAU 1,855명</span>
+                                        SEO 최적화 및 구글 검색엔진 등록, 2023년 6월 기준{' '}
+                                        <span className="hl">MAU 1,530명</span>
                                     </li>
                                     <li>기술 스택</li>
                                     <li className="ml-5">BackEnd</li>
@@ -320,7 +319,7 @@ const ResumePage = () => {
                         </div>
                         <div className="flex md:flex-row flex-col mb-8 pt-8">
                             <div className="md:w-1/4 w-full">
-                                <p className="text-3xl font-bold">4senc</p>
+                                <p className="text-3xl font-bold">회사소개</p>
                                 <p className="mt-4">2021.11 - 2022.01</p>
                                 <p className="leading-3 font-thin">Main Developer</p>
                             </div>
